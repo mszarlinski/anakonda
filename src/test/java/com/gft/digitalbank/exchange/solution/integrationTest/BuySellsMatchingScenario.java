b90d650efb11e6ffef7519b10064c038ca0301d8
Maciej Szarlinski <mszarlinski@bravurasolutions.com>
2016-07-11 12:07:05 +0200

    Buy - sells perf test

--- src/test/java/com/gft/digitalbank/exchange/solution/integrationTest/BuySellsMatchingScenario.java
+++ src/test/java/com/gft/digitalbank/exchange/solution/integrationTest/BuySellsMatchingScenario.java
@@ -1,18 +1,17 @@
 package com.gft.digitalbank.exchange.solution.integrationTest;
 
 import static java.util.Collections.emptySet;
+import static java.util.stream.Collectors.toSet;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 import com.gft.digitalbank.exchange.model.OrderBook;
 import com.gft.digitalbank.exchange.model.OrderDetails;
 import com.gft.digitalbank.exchange.model.Transaction;
 import com.gft.digitalbank.exchange.model.orders.BrokerMessage;
-import com.gft.digitalbank.exchange.model.orders.ModificationOrder;
 import com.gft.digitalbank.exchange.model.orders.PositionOrder;
 import com.gft.digitalbank.exchange.model.orders.Side;
 import com.gft.digitalbank.exchange.verification.scenario.Scenario;
@@ -20,7 +19,7 @@ import com.gft.digitalbank.exchange.verification.scenario.Scenario;
 /**
  * @author mszarlinski on 2016-07-09.
  */
-class OrderModificationsScenario extends Scenario {
+class BuySellsMatchingScenario extends Scenario {
 
     private static final int NUM_OF_BATCHES = 10_000;
 
@@ -31,10 +30,10 @@ class OrderModificationsScenario extends Scenario {
 
     @Override
     public Set<Transaction> transactions() {
-        return IntStream.rangeClosed(1, NUM_OF_BATCHES)
-            .mapToObj(x -> Transaction.builder().id(x).amount(900).price(500)
-                .brokerBuy("broker").brokerSell("broker").clientBuy("client").clientSell("client").product("A").build())
-            .collect(Collectors.toSet());
+        return IntStream.rangeClosed(1, 2 * NUM_OF_BATCHES)
+            .mapToObj(x -> Transaction.builder().id(x).amount(500).price(1000)
+                .brokerBuy("broker1").brokerSell("broker2").clientBuy("client1").clientSell("client2").product("A").build())
+            .collect(toSet());
     }
 
     @Override
@@ -48,22 +47,25 @@ class OrderModificationsScenario extends Scenario {
         List<BrokerMessage> messages = new ArrayList<>();
 
         for (int batch = 0; batch < NUM_OF_BATCHES; batch++) {
-            int sellOrderId = orderId;
+
             messages.add(
-                PositionOrder.builder().id(sellOrderId).side(Side.SELL).timestamp(sellOrderId)
-                    .client("client").broker("broker").product("A")
+                PositionOrder.builder().id(orderId).side(Side.BUY).timestamp(orderId)
+                    .client("client1").broker("broker1").product("A")
                     .details(OrderDetails.builder().amount(1000).price(1000).build()).build());
 
             orderId++;
+
             messages.add(
-                ModificationOrder.builder().id(orderId).modifiedOrderId(sellOrderId).timestamp(orderId).broker("broker")
-                    .details(OrderDetails.builder().amount(900).price(500).build()).build());
+                PositionOrder.builder().id(orderId).side(Side.SELL).timestamp(orderId)
+                    .client("client2").broker("broker2").product("A")
+                    .details(OrderDetails.builder().amount(500).price(500).build()).build());
 
             orderId++;
+
             messages.add(
-                PositionOrder.builder().id(orderId).side(Side.BUY).timestamp(orderId)
-                    .client("client").broker("broker").product("A")
-                    .details(OrderDetails.builder().amount(900).price(500).build()).build());
+                PositionOrder.builder().id(orderId).side(Side.SELL).timestamp(orderId)
+                    .client("client2").broker("broker2").product("A")
+                    .details(OrderDetails.builder().amount(500).price(500).build()).build());
         }
 
         return messages;
